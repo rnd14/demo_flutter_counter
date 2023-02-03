86183a52fcea4b088bd1347e7bafb1fe8f8772e5
14 <rnd14@detectors.pk>
2023-02-03 15:57:37 +0500

    center line changes

--- lib/main.dart
+++ lib/main.dart
@@ -48,7 +48,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed agamin:',
+              'You have pushed again number is:',
             ),
             Text(
               '$_counter',
